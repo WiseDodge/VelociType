11b1558fbb60dad1391cab37cf564d48c994147d
WiseDodge <wisedodgecoding@gmail.com>
2025-07-05 14:41:35 -0400

    Initial Commit

---
+++ src/client/java/dev/wisedodge/velocitype/client/VelocitypeClient.java
@@ -0,0 +1,10 @@
+package dev.wisedodge.velocitype.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VelocitypeClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+  }
+}
---
+++ src/main/java/dev/wisedodge/velocitype/Velocitype.java
@@ -0,0 +1,10 @@
+package dev.wisedodge.velocitype;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Velocitype implements ModInitializer {
+
+  @Override
+  public void onInitialize() {
+  }
+}
